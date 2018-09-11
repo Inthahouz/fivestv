7a441b316d0314b3e41ad5cc3b2e9250840d14b9
Nicolas Bonnamour <nicolas@8oclock.fr>
2018-09-11 13:35:11 +0200

    News route

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,10 +8,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="default")
+     * @Route("/", name="index")
      */
     public function index()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/news", name="news")
+     */
+    public function news()
+    {
+        return $this->render('default/news.html.twig');
     }
 }
